c1f9afff84136320ef02026796e43f12b7423443
Aurélien Bertron <aurelienbertron@gmail.com>
2022-11-26 11:20:38 +0100

    feat: use modules API instead of cli

--- bin/osef.js
+++ bin/osef.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
+/* eslint-disable no-magic-numbers */
+
+import { argv, exit } from "node:process";
 
 import("../lib/index.js")
-  .then(({ default: cleaner }) => cleaner({ cwd: process.cwd() }))
-  .then((success) => {
-    if (success) {
+  .then(async ({ default: run, check }) => {
+    if (argv[2] === "--checkprocess") {
+      const success = await check(argv.slice(3));
+      return { checkprocess: true, success };
+    }
+    const success = await run({ cwd: process.cwd() });
+    return { checkprocess: false, success };
+  })
+  .then(({ checkprocess, success }) => {
+    if (checkprocess) {
+      if (success) {
+        exit(0);
+      } else {
+        exit(1);
+      }
+    } else if (success) {
       console.log("Success!\n");
     } else {
       console.error("Error: cleaner failed\n");
@@ -15,5 +31,6 @@ import("../lib/index.js")
      */
     (err) => {
       console.error(`Error: ${err.message}\n`);
+      exit(1);
     }
   );
